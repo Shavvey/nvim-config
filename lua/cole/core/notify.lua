5b173a981283fd73230e71343fb65f3de7506e4e
Cole Johnson <colejohnson@student.nmt.edu>
2023-04-10 10:58:15 -0600

    added notify.lua, supresses unwanted warnings

---
+++ lua/cole/core/notify.lua
@@ -0,0 +1,9 @@
+-- Supress warning message for multiple different client offsets, I dont care about this
+local notify = vim.notify
+vim.notify = function(msg, ...)
+	if msg:match("warning: multiple different client offset_encodings") then
+		return
+	end
+
+	notify(msg, ...)
+end
