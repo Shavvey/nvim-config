2a650c2691c80c5a3bca7a03ac4196fbafdafd66
Cole Johnson <coledjohn@gmail.com>
2023-05-21 16:40:12 -0600

    Deleting some toggleterm keybindings

--- lua/cole/plugins/toggleterm.lua
+++ lua/cole/plugins/toggleterm.lua
@@ -27,7 +27,6 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
-	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
 	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
 	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
