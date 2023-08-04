544ba661f048082efcea79012c3c7d6daef4c0b5
Cole Johnson <coledjohn@gmail.com>
2023-08-04 15:28:28 -0600

    Deleting the transparency mode
    It doesn't work with my current terminal emulator

--- lua/main/plugins-setup.lua
+++ lua/main/plugins-setup.lua
@@ -47,40 +47,7 @@ return packer.startup(function(use)
 		end,
 		ft = { "markdown" },
 	})
-	use({
-		"xiyaowong/transparent.nvim",
-		config = function()
-			require("transparent").setup({
 
-				groups = { -- table: default groups
-					"Normal",
-					"NormalNC",
-					"Comment",
-					"Constant",
-					"Special",
-					"Identifier",
-					"Statement",
-					"PreProc",
-					"Type",
-					"Underlined",
-					"Todo",
-					"String",
-					"Function",
-					"Conditional",
-					"Repeat",
-					"Operator",
-					"Structure",
-					"LineNr",
-					"NonText",
-					"SignColumn",
-					"CursorLineNr",
-					"EndOfBuffer",
-				},
-				extra_groups = {}, -- table: additional groups that should be cleared
-				exclude_groups = {}, -- table: groups you don't want to clear
-			})
-		end,
-	})
 	-- commenting with gc
 	use("numToStr/Comment.nvim")
 
