91505b5563d5709fc1ec5d028ab53598aa93c934
Cole Johnson <coledjohn@gmail.com>
2024-04-05 12:38:11 -0600

    disable auto selection for nvim-cmp

--- lua/main/plugins/nvim-cmp.lua
+++ lua/main/plugins/nvim-cmp.lua
@@ -25,7 +25,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 		["<C-e>"] = cmp.mapping.abort(), -- close completion window
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<CR>"] = cmp.mapping.confirm({ select = false }), -- disable autoselection
 		["<TAB>"] = cmp.mapping(function(fallback)
 			if luasnip.jumpable(1) then -- jumps to next placeholder in the snippet
 				luasnip.jump(1)
