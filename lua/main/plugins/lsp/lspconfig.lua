c914d5e49028cb5244df71f445916757326dab2a
Cole Johnson <coledjohn@gmail.com>
2024-07-02 12:52:59 -0600

    changing info, warn, and error signs

--- lua/main/plugins/lsp/lspconfig.lua
+++ lua/main/plugins/lsp/lspconfig.lua
@@ -51,7 +51,7 @@ end
 -- used to enable autocompletion (assign to every lsp server config)
 local capabilities = cmp_nvim_lsp.default_capabilities()
 -- Change the Diagnostic symbols in the sign column (gutter)
-local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+local signs = { Error = "E ", Warn = "W ", Hint = "H ", Info = "I " }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
