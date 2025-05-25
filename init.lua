ee1480851888761d8d17e877aad16cbeb85a1695
Cole Johnson <coledjohn@gmail.com>
2025-05-25 11:22:26 -0600

    delete snippets and add comments

--- init.lua
+++ init.lua
@@ -1,15 +1,22 @@
 --import core lua files to init
 require("main.plugins-setup")
+
+-- core settings and keymaps
 require("main.core.options")
 require("main.core.keymaps")
+
+-- colorscheme and other aesthetic config stuff
 require("main.core.colorscheme")
+
+-- some more personal configuration of plugins
 require("main.plugins.comment")
 require("main.plugins.nvim-tree")
 require("main.plugins.lualine")
 require("main.plugins.nvim-cmp")
--- adding lsp support
-require("main.plugins.lsp.lsp-config")
 require("main.plugins.autopairs")
 require("main.plugins.gitsigns")
 require("main.plugins.treesitter")
 require("main.plugins.toggleterm")
+
+-- adding lsp support
+require("main.plugins.lsp.lsp-config")
