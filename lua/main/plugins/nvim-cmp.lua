366d3fc959b04e64143c084badc1095a36b0d19b
Cole Johnson <coledjohn@gmail.com>
2025-06-29 19:54:17 -0600

    commenting out additional cmp capabilities

--- lua/main/plugins/nvim-cmp.lua
+++ lua/main/plugins/nvim-cmp.lua
@@ -44,7 +44,6 @@ cmp.setup({
 		{ name = "buffer" }, -- text within current buffer
 		{ name = "path" }, -- file system paths
 	}),
-
   -- set up lspconfig
-  capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- capabilities = require('cmp_nvim_lsp').default_capabilities()
 })
