cbf8447b8516ff3cf622ae036020e02cce1e4499
Cole Johnson <coledjohn@gmail.com>
2026-06-14 19:44:37 -0600

    Fix: update treesitter plugin to use main branch

--- lua/main/plugins-setup.lua
+++ lua/main/plugins-setup.lua
@@ -158,10 +158,8 @@ return packer.startup(function(use)
 	-- treesitter configuration
 	use({
 		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
+    branch = 'main',
+    run = ':TSUpdate'
 	})
 
 	-- auto closing
